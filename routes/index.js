00726e953e5508281f8d026efd97ede39a584cfd
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-11-03 16:17:06 +0000

    Fetching weather based on query

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const url = require('url')
 const express = require('express')
 const needle = require('needle')
 const router = express.Router()
@@ -9,13 +10,22 @@ const API_KEY_VALUE = process.env.API_KEY_VALUE
 
 router.get('/', async (req, res) => {
   try {
-    const apiRes = await needle('get', `${API_BASE_URL}`)
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
     const data = apiRes.body
+    res.status(200).json(data)
+
+    // Logging the request to the public API
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
   } catch (error) {
     res.status(500).json({ error })
   }
-
-  res.status(200).json(data)
 })
 
 module.exports = router
